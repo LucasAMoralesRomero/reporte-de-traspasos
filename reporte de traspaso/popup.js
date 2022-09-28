c2937ddd3264e194bd6bcd3d4606322acd62743d
Lucas Agustín Morales Romero <moralesromerolucasagustin@gmail.com>
2022-09-28 09:40:22 -0300

    added a event listener to the button to open a new tab with the page report

--- reporte de traspaso/popup.js
+++ reporte de traspaso/popup.js
@@ -1,13 +1,14 @@
-console.log("extension corriendo...");
-//obtenemos el boton de reporte
-const imprimirReporte = document.getElementById("imprimirReporte");
 //obtenemos numero de traspaso
 let nroTraspaso = document.getElementById("nroTraspaso");
 //obtenemos numero de departamento
 let nroDepartamento = document.getElementById("nroTraspaso");
 //cuando el boton es presionado,abrimos una nueva pestaña con el traspaso
-
-function openTab(){
-    var newTab = browser.tabs.create({url:'http://aplicaciones/recepciones/REPORTESGENERALES/TraspasosSobFalt2.asp?mvNumtras='+nroTraspaso+'&mvCgruempr=&mvdepto='+nroDepartamento+'&mvsuc=184',active:true})
-    
-}
+//agregamos event listener al boton
+window.addEventListener('DOMContentLoaded', function(){
+    var link = this.document.getElementById('imprimirReporte');
+    link.addEventListener('click', function(){
+        var newURL = "http://aplicaciones/recepciones/REPORTESGENERALES/TraspasosSobFalt2.asp?mvNumtras="+nroTraspaso+"&mvCgruempr=&mvdepto="+nroDepartamento+"&mvsuc=184";
+        chrome.tabs.create({url: newURL})
+    })
+})
+console.log("extension corriendo...");
