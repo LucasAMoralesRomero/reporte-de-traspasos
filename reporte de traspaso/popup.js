ac7f46b0077bedfd6a1e92edee71ce4b58a0f770
Lucas Morales <moralesromerolucasagustin@gmail.com>
2022-09-30 02:17:16 -0300

    day 3: added into the event listener the 'get element by id' to obtain the value of nroTraspaso and nroDepartamento

--- reporte de traspaso/popup.js
+++ reporte de traspaso/popup.js
@@ -1,13 +1,18 @@
-//obtenemos numero de traspaso
-let nroTraspaso = document.getElementById("nroTraspaso");
-//obtenemos numero de departamento
-let nroDepartamento = document.getElementById("nroTraspaso");
+
+
+
 //cuando el boton es presionado,abrimos una nueva pestaña con el traspaso
 //agregamos event listener al boton
 window.addEventListener('DOMContentLoaded', function(){
+//obtenemos los elementos del formulario (nro de traspaso, departamento)
+    var nroTraspaso = document.getElementById("nroTraspaso");
+    var nroDepartamento =this.document.getElementById("nroDepartamento");
+
     var link = this.document.getElementById('imprimirReporte');
     link.addEventListener('click', function(){
-        var newURL = "http://aplicaciones/recepciones/REPORTESGENERALES/TraspasosSobFalt2.asp?mvNumtras="+nroTraspaso+"&mvCgruempr=&mvdepto="+nroDepartamento+"&mvsuc=184";
+        event.preventDefault();
+        //pasamos a la funcion la direccion de reportes, junto con los valores de traspaso y departamento (obteniendo los valores alojadas en las variables)
+        var newURL = "http://aplicaciones/recepciones/REPORTESGENERALES/TraspasosSobFalt2.asp?mvNumtras="+nroTraspaso.value+"&mvCgruempr=&mvdepto="+nroDepartamento.value+"&mvsuc=184";
         chrome.tabs.create({url: newURL})
     })
 })
